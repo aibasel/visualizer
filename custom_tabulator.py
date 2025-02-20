1c0f1c775788394be3a5b227cd41f2a79c4f069e
Salomé Eriksson <salome.eriksson@unibas.ch>
2025-02-20 16:53:22 +0100

    Add custom tabulator to avoid table scrolling to top after styling.

---
+++ custom_tabulator.py
@@ -0,0 +1,9 @@
+import panel as pn
+
+#
+class CustomTabulator(pn.widgets.Tabulator):
+    def _update_style(self, recompute=True):
+        super()._update_style(recompute=recompute)
+        self.selection = []
+        # We don't need to restore the actual selection since this seems to
+        # trigger automatically after styling
